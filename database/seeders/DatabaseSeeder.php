ce5894e4d946e783a912a2f3ada864cfc6ef8898
Bekhouche Mouadh <bekhouche.mouadh@gmail.com>
2021-09-04 21:41:08 +0100

    fixed some bugs*

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Appointment;
 use App\Models\Doctor;
 use App\Models\Laboratory;
 use App\Models\Patient;
+use App\Models\Speciality;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -21,6 +22,102 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolePermissionSeeder::class);
         $this->call(UserSeeder::class);
+
+        $specialities = [
+            "Allergologue",
+            "Anatomo-cyto-pathologiste",
+            "Andrologue",
+            "Anesthésiste-réanimateur",
+            "Angiologue",
+            "Audioprothésiste",
+            "Cancérologue",
+            "Cancérologue médical",
+            "Cancérologue radiothérapeute",
+            "Cardiologue",
+            "Chiropracteur",
+            "Chirurgien",
+            "Chirurgien cancérologue",
+            "Chirurgien de la face et du cou",
+            "Chirurgien de la main",
+            "Chirurgien infantile",
+            "Chirurgien maxillo-facial",
+            "Chirurgien maxillo-facial et stomatologue",
+            "Chirurgien oral",
+            "Chirurgien orthopédiste",
+            "Chirurgien plasticien et esthétique",
+            "Chirurgien thoracique et cardio-vasculaire",
+            "Chirurgien urologue",
+            "Chirurgien vasculaire",
+            "Chirurgien viscéral et digestif",
+            "Chirurgien-dentiste",
+            "Dermatologue",
+            "Diététicien",
+            "Endocrinologue",
+            "Ergothérapeute",
+            "Gastro-entérologue et hépatologue",
+            "Généticien",
+            "Gériatre",
+            "Gynécologue médical",
+            "Gynécologue médical et obstétrique",
+            "Gynécologue obstétricien",
+            "Hématologue",
+            "Hypnothérapeute",
+            "Infectiologue",
+            "Infirmier",
+            "Masseur-kinésithérapeute",
+            "Médecin acupuncteur",
+            "Médecin addictologue",
+            "Médecin biologiste",
+            "Médecin bucco-dentaire",
+            "Médecin du sport",
+            "Médecin du travail",
+            "Médecin généraliste",
+            "Médecin homéopathe",
+            "Médecin morphologue et anti-âge",
+            "Médecin nucléaire",
+            "Médecin nutritionniste",
+            "Médecin réanimateur",
+            "Médecin spécialiste de santé publique",
+            "Médecin urgentiste",
+            "Naturopathe",
+            "Néphrologue",
+            "Neurochirurgien",
+            "Neurologue",
+            "Ophtalmologue",
+            "Opticien-lunetier",
+            "ORL",
+            "ORL - Chirurgien de la face et du cou",
+            "Orthodontiste",
+            "Orthopédiste-orthésiste",
+            "Orthopédiste-orthésiste-podologiste",
+            "Orthophoniste",
+            "Orthoptiste",
+            "Ostéopathe",
+            "Pédiatre",
+            "Pédicure-podologue",
+            "Pharmacien",
+            "Phlébologue",
+            "Pneumologue",
+            "Psychanalyste",
+            "Psychiatre",
+            "Psychiatre de l'enfant et de l'adolescent",
+            "Psychologue",
+            "Psychomotricien",
+            "Radiologue",
+            "Radiothérapeute",
+            "Rhumatologue",
+            "Sage-femme",
+            "Sophrologue",
+            "Spécialiste en hémobiologie-transfusion",
+            "Spécialiste en médecine interne",
+            "Spécialiste en médecine légale et expertises médicales",
+            "Spécialiste en médecine physique et de réadaptation",
+            "Stomatologue",
+        ];
+
+        foreach ($specialities as $speciality) {
+            Speciality::create(["name" => $speciality]);
+        }
 
         Patient::factory()
             ->count(100)
